9d3f564d820968fb4b3481e9eaf1d0321fe3cb40
PraveenSuggula <you@example.compraveenkumar.suggula0@gmail.com>
2024-03-14 18:26:36 -0500

    updated routes

--- click2LearnInstructorClient/src/app/app-routing.module.ts
+++ click2LearnInstructorClient/src/app/app-routing.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { ShopComponent } from './sidebar/shop/shop.component';
+import { ContactComponent } from './contact/contact.component';
+import { CartComponent } from './sidebar/cart/cart.component';
+import { CoursesComponent } from './courses/courses.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'course', component: CoursesComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'cart', component: CartComponent },
   { path: '**', redirectTo: '/login' },
 ];
 
