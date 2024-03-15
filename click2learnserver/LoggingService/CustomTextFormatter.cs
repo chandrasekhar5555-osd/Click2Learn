ddbcd695cda7c3b4501ab198e1bdeacd21ecbedf
PraveenSuggula <you@example.compraveenkumar.suggula0@gmail.com>
2024-03-14 20:04:05 -0500

    Implemented base version of serilog with example error in program file

---
+++ click2learnserver/LoggingService/CustomTextFormatter.cs
@@ -0,0 +1,32 @@
+﻿using Serilog.Events;
+using Serilog.Formatting;
+
+namespace LoggingService
+{
+    public class CustomTextFormatter: ITextFormatter
+    {
+        public void Format(LogEvent logEvent, TextWriter output)
+        {
+            if(logEvent.Level.ToString() != "Information")
+            {
+                output.WriteLine("------------------------------------------------------------------------------------");
+                output.WriteLine($"Timestamp - {logEvent.Timestamp} | Level - {logEvent.Level} |");
+                output.WriteLine("------------------------------------------------------------------------------------");
+                foreach (var item in logEvent.Properties)
+                {
+                    output.WriteLine(item.Key + " : " + item.Value);
+                }
+                if(logEvent.Exception != null)
+                {
+                    output.WriteLine("------------------------------ EXCEPTION DETAILS --------------------------------");
+                    output.WriteLine("Exception - {0}", logEvent.Exception);
+                    output.WriteLine("StackTrace - {0}", logEvent.Exception.StackTrace);
+                    output.WriteLine("Message - {0}", logEvent.Exception.Message);
+                    output.WriteLine("Source - {0}", logEvent.Exception.Source);
+                    output.WriteLine("InnerException - {0}", logEvent.Exception.InnerException);
+                }
+                output.WriteLine("------------------------------------------------------------------------------------");
+            }
+        }
+    }
+}
--- click2learnserver/click2learnserver/Program.cs
+++ click2learnserver/click2learnserver/Program.cs
@@ -1,13 +1,35 @@
+using LoggingService;
+using Serilog;
+using System.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configure Serilog
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .Enrich.WithProperty("Application", "click2learnserver")
+    .Enrich.WithProperty("MachineName", Environment.MachineName)
+    .Enrich.WithProperty("CurrentManagedThreadId", Environment.CurrentManagedThreadId)
+    .Enrich.WithProperty("OSVersion", Environment.OSVersion)
+    .Enrich.WithProperty("Version", Environment.Version)
+    .Enrich.WithProperty("UserName", Environment.UserName)
+    .Enrich.WithProperty("ProcessId", Process.GetCurrentProcess().Id)
+    .Enrich.WithProperty("ProcessName", Process.GetCurrentProcess().ProcessName)
+    .WriteTo.Console()
+    .WriteTo.File(new CustomTextFormatter(), Path.Combine(builder.Environment.ContentRootPath, "Logs", $"click2learn_server_report_{DateTime.Now:yyyyMMdd}.txt"))
+    .CreateLogger();
 
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Configure logging
+app.UseSerilogRequestLogging();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,5 +43,16 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    // Intentional division by zero to create an exception
+    int zero = 0;
+    int result = 100 / zero;
+}
+catch (Exception ex)
+{
+    Log.Error(ex, "An error occurred: {ErrorMessage}", ex.Message);
+}
 
 app.Run();
