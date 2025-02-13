58bf00b1d16e33799230e753098c8210c6449094
chandrasekhar5555-osd <pramodneelam918.com>
2025-02-13 00:02:49 -0600

    updated db connection method

--- click2LearnBackend/database.js
+++ click2LearnBackend/database.js
@@ -1,5 +1,17 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
-// Connect to MongoDB
-mongoose.connect(
-    "mongodb+srv://chandrasekharn081998:0jrxKojzbuipDxHD@click2learn.sqpcc.mongodb.net/?retryWrites=true&w=majority&appName=Click2Learn"
-  );
+
+const connectDB = async () => {
+    try {
+        const uri = process.env.MONGO_URI;
+        console.log("MongoDB Connecting...");
+        await mongoose.connect(uri);
+        console.log("MongoDB Connected...");
+    } catch (error) {
+        console.error("MongoDB connection failed:", error.message);  // Log just the message
+        console.error("Stack Trace:", error.stack);  // Log the full stack trace
+        process.exit(1); // Exit if connection fails
+    }
+};
+connectDB();    
+
